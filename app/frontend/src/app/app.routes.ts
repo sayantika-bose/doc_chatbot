4ad5096be27524bf5d2e3924e39ca765517def5e
Sai-Sudhane <saidon7321@gmail.com>
2025-04-24 20:08:33 +0530

    chore:minor fixes

--- app/frontend/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-  { path: 'upload', loadComponent: () => import('./features/upload/upload.component').then(m => m.UploadComponent) },
-  { path: 'chat', loadComponent: () => import('./features/chat/chat.component').then(m => m.ChatComponent) },
-  { path: '', redirectTo: '/upload', pathMatch: 'full' }
-];
